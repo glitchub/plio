ac300acf41820aa56d9e87a381e54a31054569a0
glitchub <glitchub@users.noreply.github.com>
2019-12-12 15:56:14 -0800

    Add specified gpio subclass

--- i2c_tca6408.py
+++ i2c_tca6408.py
@@ -3,13 +3,13 @@
 from i2c import i2c
 from time import sleep
 
-class tca6408():
+class tca6408:
 
-    # There are 4 eight-bit registers
-    IN  = 0 # 1 = logical input states
-    OUT = 1 # 1 = buffered output states
-    INV = 2 # 1 = invert the pin's physical state
-    DIR = 3 # aka configuration register, 1 = output
+    # Registers have one bit per pin
+    IN  = 0 # 1 = input pin is logic high
+    OUT = 1 # 1 = output pin is logic high
+    INV = 2 # 1 = pin logical state is inverse of physicall
+    DIR = 3 # 1 = pin is input
 
     def __init__(self, bus, addr=0x20):
         self.addr = addr
@@ -17,46 +17,61 @@ class tca6408():
         self.cache = {}
 
     # Set or clear masked bits in specified register to specified value
-    # Values are cached, use only for config registers
+    # Values are cached!
     def config(self, reg, mask, value):
         assert 1 <= reg <= 3 and 1 <= mask <= 0xff
         if reg not in self.cache: self.cache[reg] = self.i2c.io(reg,1)[0][0]
         r = (self.cache[reg] & ~mask) | (value & mask)
         if r != self.cache[reg]:
-            self.cache[reg]=r
+            self.cache[reg] = r
             self.i2c.io([reg, r])
 
-    # change masked gpios to inputs and return set bits.
-    # If invert flag is not None, update the inversion register.
-    def get_mask(self, mask):
-        self.config(self.DIR, mask, mask) # change to inputs
-        return self.i2c.io([self.IN, 1])[0][0] & mask
+    # change masked gpios to inputs and return their states
+    def input(self, mask):
+        self.config(self.DIR, mask, mask)               # change to inputs
+        return self.i2c.io([self.IN, 1])[0][0] & mask   # return masked states
 
-    # change masked gpios to outputs and drive them to specified state.
-    def set_mask(self, mask, state):
-        self.config(self.DIR, mask, 0) # change to outputs
-        self.config(self.OUT, mask, mask if state else 0)
+    # change masked gpios to outputs and set them to specified state.
+    def output(self, mask, states):
+        self.config(self.DIR, mask, 0)                  # change to outputs
+        self.config(self.OUT, mask, states)             # set masked states
 
-    # Change specified gpio 0-7 to a input and return its state.
-    def get_gpio(self, gpio):
-        assert 0 <= gpio <= 7
-        return bool(self.get_mask(1 << gpio))
+    # set inversion for masked gpios (whether input or output)
+    def invert(self, mask, states):
+        self.config(self.INV, mask, states)             # set inversion states
 
-    # Change specified gpio 0-7 to an output and set its state.
-    def set_gpio(self, gpio, state):
-        assert 0 <= gpio <= 7
-        self.set_mask(1 << gpio, state)
-
-    # Reset to non-inverted inputs
+    # reset to HI-Z, disable inversion
     def reset(self):
-        self.get_mask(0xFF)
+        self.input(0xFF)
+        self.invert(0xFF, 0x00)
+
+    # private gpio class
+    class __gpio:
+        def __init__(self, tca6408, gpio):
+            assert 0 <= gpio <= 7
+            self.gpio = 1 << gpio
+            self.tca6408 = tca6408
+        def output(self, state): self.tca6408.output(self.gpio, 0xFF if state else 0)   # Change the gpio to output and set its state
+        def input(self): return bool(self.tca6408.input(self.gpio))                     # Change the gpio to input and return its state
+        def invert(self, state): self.tca6408.invert(self.gpio, 0xFF if state else 0)   # Set the gpio inversion
+
+    # return a __gpio
+    def gpio(self, gpio): return self.__gpio(self, gpio)
 
 if __name__ == "__main__":
     chip = tca6408(0x21)
 
-    # get all gpios in parallel
-    print "Input state = 0x02X" % chip.get_mask(0xFF)
+    chip.reset()
 
-    # get them one at a time
-    for i in range(0,8):
-        print "gpio %d = %r" % (i, chip.get_gpio(i))
+    # get current input states
+    print "Inputs = 0x02X" % chip.input(0xFF)
+
+    # fiddle with discrete gpios
+    g0=chip.gpio(0)             # g0 is pin 0
+    g1=chip.gpio(1)             # g1 is pin 1
+    g2=chip.gpio(2)             # g2 is pin 2
+
+    g0.output(1)                # set g0
+    g1.invert(1)                # invert g1
+    g1.output(0)                # clear g1, which actually sets it
+    print "g2 is ",g2.input()   # report g2
